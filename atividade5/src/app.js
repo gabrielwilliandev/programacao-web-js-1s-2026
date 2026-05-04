5c09edb0685203be49cfef544894245a7eda3132
GABRIEL WILLIAN DOS SANTOS SOUZA <gabriel.willian@a.ucb.br>
2026-05-04 20:21:21 -0300

    Adicionando mustache

--- atividade5/src/app.js
+++ atividade5/src/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
 const path = require('path');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, '../view')));
 
-app.post('/enviar-dados', (req, res) => {
-    const {nome, sobrenome, datanasc, telefone, cep, endereco, clinica, especialidade, dataconsulta, horario, observacao} = req.body;
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '../view'));
 
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+
+app.post('/agendamento', (req, res) => {
+    const {nome, sobrenome, datanasc, telefone, cep, endereco, clinica, especialidade, dataconsulta, horario, observacao} = req.body;
+    
     for (let campo in req.body) {
         if (campo !== 'observacao' && !req.body[campo]) {
             return res.status(400).send(`O campo ${campo} é obrigatório.`);
@@ -25,7 +34,9 @@ app.post('/enviar-dados', (req, res) => {
     if (dataAgendamento <= dataAtual) {
         return res.status(400).send('Erro: A data da consulta deve ser superior à data de hoje.');
     }
-    res.send(`Sucesso! Agendamento realizado para ${nome} ${sobrenome} na clínica ${clinica} no dia ${dataconsulta} às ${horario}.`);
+    res.render('agendamento.html', {
+        dados_agendamento: req.body
+    });
 });
 
 const PORT = process.env.PORT || 3000;
