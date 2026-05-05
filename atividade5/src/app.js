09682a9f222fd9a0e5d35b0fe567ccc5c5fcedc0
gabrielwilliandev <contatogwill@gmail.com>
2026-05-04 21:05:44 -0300

    Adicionando erros

--- atividade5/src/app.js
+++ atividade5/src/app.js
@@ -4,38 +4,65 @@ const path = require('path');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../view')));
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, '../view'));
 
 app.get('/', (req, res) => {
-    res.render('index.html');
+    res.render('index.html', {
+        erros: [],
+        dados: {}
+    });
 });
 
 
 app.post('/agendamento', (req, res) => {
-    const {nome, sobrenome, datanasc, telefone, cep, endereco, clinica, especialidade, dataconsulta, horario, observacao} = req.body;
-    
-    for (let campo in req.body) {
-        if (campo !== 'observacao' && !req.body[campo]) {
-            return res.status(400).send(`O campo ${campo} é obrigatório.`);
-        }
+
+    const dados = req.body;
+    const erros = [];
+
+    const camposObrigatorios = [
+    'nome',
+    'sobrenome',
+    'cpf',
+    'datanasc',
+    'telefone',
+    'cep',
+    'endereco',
+    'clinica',
+    'especialidade',
+    'dataconsulta',
+    'horario'
+];
+
+camposObrigatorios.forEach(campo => {
+    if (!dados[campo]) {
+        erros.push(`O campo ${campo} é obrigatório.`);
+    }
+});
+
+    if (erros.length > 0) {
+        return res.render('index.html', {
+            erros,
+            dados
+        });
     }
 
+    dados.isCardio = dados.especialidade === "cardiologia";
+    dados.isDerma = dados.especialidade === "dermatologia";
+    dados.isNeuro = dados.especialidade === "neurologia";
 
-    const dataAgendamento = new Date(dataconsulta);
+    const dataAgendamento = new Date(dados.dataconsulta + 'T00:00:00');
     const dataAtual = new Date();
-    
     dataAtual.setHours(0, 0, 0, 0);
-    dataAgendamento.setMinutes(dataAgendamento.getTimezoneOffset());
 
     if (dataAgendamento <= dataAtual) {
         return res.status(400).send('Erro: A data da consulta deve ser superior à data de hoje.');
     }
+
     res.render('agendamento.html', {
-        dados_agendamento: req.body
+        dados_agendamento: dados
     });
 });
 
